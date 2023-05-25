4c8da39d9e55410805523842428e06d28534f5fe
Bobur1545 <babajonovboburbek@gmail.com>
2023-05-25 16:49:26 +0500

    bitta saytdagi malumotlarni chiqarish uchun

--- index.php
+++ index.php
@@ -40,35 +40,74 @@ foreach ($html->find('#news-list a') AS $element){
 //yuqoridagi topib olingan saytlardan faqat bittasini tutib olish uchun
 foreach ($posts as $post) {
     $birinchi_sayt[] = SITE_URL . $post['link'];
-    $link1 = $birinchi_sayt[0];
-    break;
+//    $link1 = $birinchi_sayt[2];
+//    break;
+}
+
+foreach ($birinchi_sayt as $link1) {
+    $page_into_site = curlGetPage($link1);
+    $html_into_site = str_get_html($page_into_site);
+
+    $posts_into_site = [];
+    foreach ($html_into_site->find('.single-layout__center') as $element_site) {
+        $date = $element_site->find('.date', 0);
+        $title = $element_site->find('.single-header__title', 0);
+        $img = $element_site->find('.main-img img', 0);
+        $text = $element_site->find('.single-content', 0);
+
+        $data = [
+            'date' => ($date) ? $date->plaintext : '',
+            'title' => ($title) ? $title->plaintext : '',
+            'img' => ($img) ? $img->src : '',
+            'text' => ($text) ? $text->plaintext : '',
+        ];
+
+        if (!$data['date'])
+            continue;
+
+        $posts_into_site[] = $data;
+    }
+
+    // Ma'lumotlarni saqlash yoki ko'rsatish kabi $posts_into_site bilan biror narsa qilish
+
+    // Shunnab chiqarsa boladi: Joriy $link1 uchun ma'lumotlarni chop etish
+    echo "Data for $link1:<br>";
+    foreach ($posts_into_site as $post) {
+        echo "Date: {$post['date']}<br>";
+        echo "Title: {$post['title']}<br>";
+        echo "Image: {$post['img']}<br>";
+        echo "Text: {$post['text']}<br><br>";
+    }
+    echo "<hr>";
 }
 
 
-//o'sha tutib olingan saytdagi malumotlarni ekranga chiqarish uchun
-$page_into_site = curlGetPage($link1);
-$html_into_site = str_get_html($page_into_site);
+//
+////o'sha tutib olingan saytdagi malumotlarni ekranga chiqarish uchun
+//    $page_into_site = curlGetPage($link1);
+//    $html_into_site = str_get_html($page_into_site);
+//
+//    $posts_into_site = [];
+//    foreach ($html_into_site->find('.single-layout__center') AS $element_site){
+//        $date = $element_site->find('.date', 0);
+//        $title = $element_site->find('.single-header__title', 0);
+//        $img = $element_site->find('.main-img img', 0);
+//        $text = $element_site->find('.single-content', 0);
+//
+//        $data = [
+//            'date' => $date->plaintext,
+//            'title' => $title->plaintext,
+//            'img' => $img->src,
+//            'text' => $text->plaintext,
+//        ];
+//
+//        if (!$data['date'])
+//            continue;
+//
+//        $posts_into_site[] = $data;
+//    }
 
-$posts_into_site = [];
-foreach ($html_into_site->find('.single-layout__center') AS $element_site){
-    $date = $element_site->find('.date', 0);
-    $title = $element_site->find('.single-header__title', 0);
-    $img = $element_site->find('.main-img img', 0);
-    $text = $element_site->find('.single-content p', 0);
+//echo '<pre>';
+//print_r($posts_into_site);
+//echo '</pre>';
 
-    $data = [
-        'date' => $date->plaintext,
-        'title' => $title->plaintext,
-        'img' => $img->src,
-        'text' => $text->plaintext,
-    ];
-
-    if (!$data['date'])
-        continue;
-
-    $posts_into_site[] = $data;
-}
-
-echo '<pre>';
-print_r($posts_into_site);
-echo '</pre>';
